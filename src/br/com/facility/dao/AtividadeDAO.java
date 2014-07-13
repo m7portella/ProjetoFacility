e9456a50005968d51e4f178e96c7da36fdb3e87f
Matheus Bruno Damiate <mbdamiate@gmail.com>
2014-07-13 20:42:36 -0300

    Atividade
    Correção no DAO, DAOImpl

--- src/br/com/facility/dao/AtividadeDAO.java
+++ src/br/com/facility/dao/AtividadeDAO.java
@@ -1,7 +1,11 @@
 package br.com.facility.dao;
 
+import java.util.List;
+
 import br.com.facility.to.Atividade;
 
 public interface AtividadeDAO extends DAO<Atividade, Integer> {
+
+	List<Atividade> listarTodos(Atividade a);
 
 }
--- src/br/com/facility/dao/impl/AitividadeDAOImpl.java
+++ src/br/com/facility/dao/impl/AitividadeDAOImpl.java
@@ -1,8 +1,12 @@
 package br.com.facility.dao.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import br.com.facility.dao.AtividadeDAO;
+import br.com.facility.to.Amigo;
 import br.com.facility.to.Atividade;
 
 
@@ -13,5 +17,12 @@ public class AitividadeDAOImpl extends DAOImpl<Atividade, Integer> implements At
 		// TODO Auto-generated constructor stub
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Atividade> listarTodos(Atividade a) {
+		Query q = em.createQuery("from T_ATIVIDADE a where a.atividade = :a").setParameter("a", a); //Nao conheço essa função, simplesmente substitui o codigo pelo que supus
+		List<Atividade> lista = q.getResultList();
+		return lista;
+	}
 	
 }
