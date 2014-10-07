8343d5fe01a2dc1b93049cfe87e5616bb2309335
Matheus Bruno Damiate <mbdamiate@gmail.com>
2014-10-06 22:51:41 -0300

    Alterações na Busca de Profissionais

--- src/br/com/facility/dao/ProfissionalDAO.java
+++ src/br/com/facility/dao/ProfissionalDAO.java
@@ -2,10 +2,17 @@ package br.com.facility.dao;
 
 import java.util.List;
 
+import br.com.facility.to.Atividade;
+import br.com.facility.to.Categoria;
+import br.com.facility.to.Especialidade;
 import br.com.facility.to.Profissional;
 
 public interface ProfissionalDAO extends DAO<Profissional, Integer>{
 
 	List<Profissional> listar();
-	
+	List<Profissional> buscarProfissionalPorNome(String nome);
+	List<Profissional> buscarProfissionalPorCidade(String cidade);
+	List<Profissional> buscarProfissionalPorCategoria(Categoria categoria);
+	List<Profissional> buscarProfissionalPorAtividade(Atividade atividade);
+	List<Profissional> buscarProfissionalPorEspecialidade(Especialidade especialidade);
 }
--- src/br/com/facility/dao/impl/ProfissionalDAOImpl.java
+++ src/br/com/facility/dao/impl/ProfissionalDAOImpl.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.facility.dao.ProfissionalDAO;
+import br.com.facility.to.Atividade;
+import br.com.facility.to.Categoria;
+import br.com.facility.to.Especialidade;
 import br.com.facility.to.Profissional;
 
 public class ProfissionalDAOImpl extends DAOImpl<Profissional, Integer> implements ProfissionalDAO{
@@ -19,6 +23,38 @@ public class ProfissionalDAOImpl extends DAOImpl<Profissional, Integer> implemen
 		Query q = em.createQuery("from Profissional");
 		List<Profissional> lista = q.getResultList();
 		return lista;
+	}
+
+	@Override
+	public List<Profissional> buscarProfissionalPorNome(String nome) {
+		TypedQuery<Profissional> query = em.createQuery("from Profissional p where p.nome like :nome",Profissional.class);
+		query.setParameter("nome","%"+ nome + "%");
+		return query.getResultList();
+	}
+
+	@Override
+	public List<Profissional> buscarProfissionalPorCidade(String cidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Profissional> buscarProfissionalPorCategoria(Categoria categoria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Profissional> buscarProfissionalPorAtividade(Atividade atividade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Profissional> buscarProfissionalPorEspecialidade(
+			Especialidade especialidade) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
